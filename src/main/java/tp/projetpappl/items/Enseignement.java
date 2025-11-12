84321be0ad8092887a9f27f0ecd2da983994357f
nathan <nathan@hp-nathan>
2025-11-12 16:38:06 +0100

    mise sur Git des objets du projet

---
+++ src/main/java/tp/projetpappl/items/Enseignement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp.projetpappl.items;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author nathan
+ */
+public class Enseignement {
+    private String nomEnseignement;
+    private String acronyme;
+    private String filiere;
+    private Responsable responsable;
+    private List<Constitution> constitution=new ArrayList<>(5);
+}
