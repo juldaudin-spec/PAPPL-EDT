89e4346051fa44c1a4f1f7a4af4804a6a9259529
superjasper51 <nath.degodet@gmail.com>
2025-11-26 18:06:02 +0100

    ajout des repositories pour Enseignant

---
+++ src/main/java/tp/projetpappl/repositories/EnseignantRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package tp.projetpappl.repositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import tp.projetpappl.items.Enseignant;
+/**
+ *
+ * @author nathan
+ */
+@Repository
+public interface EnseignantRepository {
+    
+}
