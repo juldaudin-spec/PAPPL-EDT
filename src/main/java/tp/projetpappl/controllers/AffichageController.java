b2b18f444f14fd9e5809067ee3b5a173a9fb87cd
superjasper51 <nath.degodet@gmail.com>
2026-01-19 17:41:11 +0100

    Ajout de la récupération des séances des groupes choisis

--- src/main/java/tp/projetpappl/controllers/AffichageController.java
+++ src/main/java/tp/projetpappl/controllers/AffichageController.java
@@ -4,9 +4,21 @@
  */
 package tp.projetpappl.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import tp.projetpappl.items.Groupe;
+import tp.projetpappl.items.Seance;
+import tp.projetpappl.repositories.GroupeRepository;
+import tp.projetpappl.repositories.SeanceRepository;
 
 /**
  *
@@ -15,10 +27,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AffichageController {
     
-    @RequestMapping(value="affichageEDT.do")
-    public ModelAndView handleIndexGet(){
-        return new ModelAndView("affichageEDT");
+    @Autowired
+    private SeanceRepository seanceRepository;
+    @Autowired
+    private GroupeRepository groupeRepository;
+    
+    @RequestMapping(value="affichageEDT.do",method=RequestMethod.POST)
+    public ModelAndView handleIndexGet(HttpServletRequest request){
+        ModelAndView returned = new ModelAndView("affichageEDT");
         
+        String[] idStr=request.getParameterValues("idGroupe");
+    
+        Optional<Groupe> result;
+        Groupe groupe;
+        List<String> listeGroupe=new ArrayList<>(idStr.length); 
+        List<Seance> myList;
+        List<List<Seance>> myList2=new ArrayList<>(idStr.length); 
+        
+        for (String elem :idStr){
+            result = groupeRepository.findById(elem);
+            if ((result.isPresent())){
+                groupe= result.get();
+                myList=groupe.getSeanceList();
+                Collections.sort(myList,Seance.getComparator());  
+                myList2.add(myList);
+                listeGroupe.add(groupe.getNomGroupe());}
+            }
+        
+        returned.addObject("listeSeance", myList2);
+        returned.addObject("listeGroupe", listeGroupe);
+        return returned;
     }
     
 }
