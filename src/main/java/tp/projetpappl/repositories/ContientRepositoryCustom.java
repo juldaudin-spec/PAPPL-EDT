df86121f4971fcbe1a804dd02c118209b9ffb5c7
superjasper51 <nath.degodet@gmail.com>
2026-01-23 17:00:07 +0100

    ajout des méthodes permettant de récupérer les informations dont on a besoin

--- src/main/java/tp/projetpappl/repositories/ContientRepositoryCustom.java
+++ src/main/java/tp/projetpappl/repositories/ContientRepositoryCustom.java
@@ -4,10 +4,19 @@
  */
 package tp.projetpappl.repositories;
 
+import java.util.List;
+import tp.projetpappl.items.Contient;
+import tp.projetpappl.items.Enseignement;
+import tp.projetpappl.items.Groupe;
+import tp.projetpappl.items.TypeLecon;
+
 /**
  *
  * @author nathan
  */
 public interface ContientRepositoryCustom {
-    
+    List<Contient> findContientByIntituleByEnseignementByGroupe(String intitule,String acronyme,String nomGroupe);
+    List<TypeLecon> findIntituleByEnseignementByGroupe(String acronyme, String nomGroupe);
+    List<Enseignement> findEnseignementByGroupe(String nomGroupe);
+    List<Groupe> findGroupeByEnseignement(String acronyme);    
 }
