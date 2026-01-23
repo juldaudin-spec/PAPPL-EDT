47b920e71be44c9fa07ea980445825e86f10adf9
superjasper51 <nath.degodet@gmail.com>
2026-01-23 08:39:43 +0100

    changement du controller pour passer les cours de chaque groupe par jour pour les mettre proprement dans les div (on récupère des sous listes triée : par jour puis par groupe)

--- src/main/java/tp/projetpappl/controllers/AffichageController.java
+++ src/main/java/tp/projetpappl/controllers/AffichageController.java
@@ -24,74 +24,80 @@ import tp.projetpappl.items.Seance;
 import tp.projetpappl.repositories.GroupeRepository;
 
 /**
- * Ceci est le controlleur pour tout ce qui concerne l'affichage de l'emplois du temps (EDT)
+ * Ceci est le controlleur pour tout ce qui concerne l'affichage de l'emplois du
+ * temps (EDT)
+ *
  * @author nathan
  */
 @Controller
 public class AffichageController {
-    
+
     @Autowired
     private GroupeRepository groupeRepository;
-    
+
     /**
      * permet d'ouvrir la page d'affichage des EDT
+     *
      * @param request
-     * @return 
+     * @return
      */
-    @RequestMapping(value="affichageEDT.do",method=RequestMethod.POST)
-    public ModelAndView handleIndexGet(HttpServletRequest request){
+    @RequestMapping(value = "affichageEDT.do", method = RequestMethod.POST)
+    public ModelAndView handleIndexGet(HttpServletRequest request) {
         ModelAndView returned = new ModelAndView("affichageEDT");
-        
-        if (request!=null){
-        String[] idStr=request.getParameterValues("idGroupe");
-        
-        List<Groupe> groupes=groupeRepository.findAll();
-        
-        Optional<Groupe> result;
-        Groupe groupe;
-        List<String> listeGroupe= null;
-        List<Seance> myList;
-        List<List<Seance>> myList2=null;
-        
-        if (idStr!=null){
-        listeGroupe=new ArrayList<>(idStr.length); 
-        myList2=new ArrayList<>(idStr.length); 
-        
-        for (String elem :idStr){
-            result = groupeRepository.findById(elem);
-            if ((result.isPresent())){
-                groupe= result.get();
-                myList=groupe.getSeanceList();
-                Collections.sort(myList,Seance.getComparator());  
-                myList2.add(myList);
-                listeGroupe.add(groupe.getNomGroupe());}
+
+        if (request != null) {
+            String[] idStr = request.getParameterValues("idGroupe");
+
+            List<Groupe> groupes = groupeRepository.findAll();
+
+            Optional<Groupe> result;
+            Groupe groupe;
+            List<String> listeGroupe = null;
+            List<Seance> myList;
+            List<List<Seance>> myList2 = null;
+
+            if (idStr != null) {
+                listeGroupe = new ArrayList<>(idStr.length);
+                myList2 = new ArrayList<>(idStr.length);
+
+                for (String elem : idStr) {
+                    result = groupeRepository.findById(elem);
+                    if ((result.isPresent())) {
+                        groupe = result.get();
+                        myList = groupe.getSeanceList();
+                        Collections.sort(myList, Seance.getComparator());
+                        myList2.add(myList);
+                        listeGroupe.add(groupe.getNomGroupe());
+                    }
+                }
             }
+
+            List<Date> listHDebut = listHDebut(myList2);
+            List<LocalDate> listDate = getTheDates(listHDebut);
+            List<List<Seance>> listSeance = transform(myList2, listHDebut);
+            List<List<Seance>> listSeance2;
+
+            returned.addObject("groupes", groupes);
+            returned.addObject("HDebut", listDate);
+            returned.addObject("listeSeance", listSeance);
+            returned.addObject("listeGroupe", listeGroupe);
         }
-        
-        List<Date> listHDebut = listHDebut(myList2);
-        List<LocalDate> listDate=getTheDates(listHDebut);
-        List<List<Seance>> listSeance = transform(myList2, listHDebut);
-        List<List<Seance>> listSeance2;
-        
-        returned.addObject("groupes", groupes);
-        returned.addObject("HDebut", listDate);
-        returned.addObject("listeSeance", listSeance);
-        returned.addObject("listeGroupe", listeGroupe);}
         return returned;
     }
-    
+
     /**
      * Permet de récupérer la liste des heures où les cours commencent
+     *
      * @param myList2
-     * @return 
+     * @return
      */
-    public List<Date> listHDebut(List<List<Seance>> myList2){
-        List<Date> listHDebut=null;
-        if (myList2!=null){
-            listHDebut=new ArrayList<>();
-            for (List<Seance> seanceGroupe : myList2){
-                for (Seance seance : seanceGroupe){
-                    if (!listHDebut.contains(seance.getHDebut())){
+    public List<Date> listHDebut(List<List<Seance>> myList2) {
+        List<Date> listHDebut = null;
+        if (myList2 != null) {
+            listHDebut = new ArrayList<>();
+            for (List<Seance> seanceGroupe : myList2) {
+                for (Seance seance : seanceGroupe) {
+                    if (!listHDebut.contains(seance.getHDebut())) {
                         listHDebut.add(seance.getHDebut());
                     }
                 }
@@ -100,84 +106,98 @@ public class AffichageController {
         }
         return listHDebut;
     }
+
     /**
-     * permet de transformer la liste qui affiche les cours à l'horizontal vers une liste qui permet d'afficher les cours à la verticale
+     * permet de transformer la liste qui affiche les cours à l'horizontal vers
+     * une liste qui permet d'afficher les cours à la verticale
+     *
      * @param myList2
      * @param listHDebut
-     * @return 
+     * @return
      */
-    public List<List<Seance>> transform(List<List<Seance>> myList2, List<Date> listHDebut){
-        List<List<Seance>> listeSeance=null;
+    public List<List<Seance>> transform(List<List<Seance>> myList2, List<Date> listHDebut) {
+        List<List<Seance>> listeSeance = null;
         List<Seance> ligne;
-        if (myList2!=null&&listHDebut!=null){
-            listeSeance=new LinkedList<>();
-            for (Date horaire : listHDebut){
+        if (myList2 != null && listHDebut != null) {
+            listeSeance = new LinkedList<>();
+            for (Date horaire : listHDebut) {
                 ligne = new ArrayList<>(myList2.size());
-                for (List<Seance> seanceGroupe : myList2){
+                for (List<Seance> seanceGroupe : myList2) {
                     addSeanceLigne(seanceGroupe, ligne, horaire);
                 }
                 listeSeance.add(ligne);
-            } 
+            }
         }
         return listeSeance;
     }
-    
+
     /**
-     * fonction qui permet d'ajouter une séance à la ligne si l'horaire de la ligne correspond à celle de la séance et que tout va bien
+     * fonction qui permet d'ajouter une séance à la ligne si l'horaire de la
+     * ligne correspond à celle de la séance et que tout va bien
+     *
      * @param seanceGroupe
      * @param ligne
-     * @param horaire 
+     * @param horaire
      */
-    public void addSeanceLigne(List<Seance> seanceGroupe, List<Seance> ligne, Date horaire){
-        if (seanceGroupe!=null&&ligne!=null){
-        if ((!seanceGroupe.isEmpty())&&horaire!=null){
-                    if (horaire.compareTo(seanceGroupe.get(0).getHDebut())==0){
-                        ligne.add(seanceGroupe.remove(0));
-                    }
-                    else{
-                        ligne.add(null);
-                    }}
-                    else{
-                        ligne.add(null);
-                    }
-                
-    }}
-    
-    public List<LocalDate> getTheDates(List<Date> listHDebut){
-        if (listHDebut!=null){
+    public void addSeanceLigne(List<Seance> seanceGroupe, List<Seance> ligne, Date horaire) {
+        if (seanceGroupe != null && ligne != null) {
+            if ((!seanceGroupe.isEmpty()) && horaire != null) {
+                if (horaire.compareTo(seanceGroupe.get(0).getHDebut()) == 0) {
+                    ligne.add(seanceGroupe.remove(0));
+                } else {
+                    ligne.add(null);
+                }
+            } else {
+                ligne.add(null);
+            }
+
+        }
+    }
+
+    public List<LocalDate> getTheDates(List<Date> listHDebut) {
+        if (listHDebut != null) {
             listHDebut.sort(Comparator.naturalOrder());
             Date date;
             LocalDate localDate;
             LocalDate newLocalDate;
-            List<LocalDate> listOfDay= new ArrayList<>();
-            for (int i=0; i<listHDebut.size();i++){
-                date =listHDebut.get(i);
-                localDate= date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            List<LocalDate> listOfDay = new ArrayList<>();
+            for (int i = 0; i < listHDebut.size(); i++) {
+                date = listHDebut.get(i);
+                localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
                 newLocalDate = LocalDate.of(2026, 1, 22);
-                if (!comparerDate(localDate,newLocalDate)){
+                if (!comparerDate(localDate, newLocalDate)) {
                     listOfDay.add(newLocalDate);
                 }
             }
-        return listOfDay;            
-        }        
+            return listOfDay;
+        }
         return null;
     }
-    
-    public boolean comparerDate(LocalDate date1, LocalDate date2){
-        return ((date1.getYear()==date2.getYear())&&(date1.getMonthValue()==date2.getMonthValue())&&(date1.getDayOfMonth()==date2.getDayOfMonth()));
+
+    public boolean comparerDate(LocalDate date1, LocalDate date2) {
+        return ((date1.getYear() == date2.getYear()) && (date1.getMonthValue() == date2.getMonthValue()) && (date1.getDayOfMonth() == date2.getDayOfMonth()));
     }
-    
-    public List<List<Seance>> groupByDates(List<LocalDate> listDates, List<List<Seance>> myList2){
-        if (listDates!=null&&myList2!=null){
-            List<List<Seance>> seanceByDay=new ArrayList<>(listDates.size());
+
+    public List<List<List<Seance>>> groupByDates(List<LocalDate> listDates, List<List<Seance>> myList2) {
+        if (listDates != null && myList2 != null) {
+            List<List<List<Seance>>> seanceByDay = new ArrayList<>(listDates.size());//par jour et par groupe
+            List<List<Seance>> seanceOfDay;
+            List<Seance> seanceOfGroupe;
             listDates.sort(Comparator.naturalOrder());
-            for (List<Seance> seanceGroupe : myList2){
-                Collections.sort(seanceGroupe, Seance.getComparator());
-                for (LocalDate date : listDates){
-                    if (comparerDate(date,))
+            for (LocalDate date : listDates) {
+                seanceOfDay = new ArrayList<>(myList2.size());
+                for (List<Seance> seanceGroupe : myList2) {
+                    seanceOfGroupe = new ArrayList<>();
+                    Collections.sort(seanceGroupe, Seance.getComparator());
+                    while (comparerDate(date, seanceGroupe.get(0).getHDebut().toInstant().atZone(ZoneId.systemDefault()).toLocalDate())) {
+                        seanceOfGroupe.add(seanceGroupe.remove(0));
+                    }
+                    seanceOfDay.add(seanceOfGroupe);
                 }
+                seanceByDay.add(seanceOfDay);
             }
-        }        
+            return seanceByDay;
+        }
         return null;
     }
 }
