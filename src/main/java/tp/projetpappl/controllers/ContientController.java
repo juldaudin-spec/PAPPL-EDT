e3c41a3ffbe983b2b19f4bc37f06d4546fb8ac32
superjasper51 <nath.degodet@gmail.com>
2026-02-02 15:21:47 +0100

    version presque fonctionnelle (on a un affichage du temps programmé pour les cours présents dans la maquette, avec le temps dans la maquette)

--- src/main/java/tp/projetpappl/controllers/ContientController.java
+++ src/main/java/tp/projetpappl/controllers/ContientController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,9 +18,12 @@ import static tp.projetpappl.controllers.Tools.getIntFromString;
 import tp.projetpappl.items.Contient;
 import tp.projetpappl.items.Enseignement;
 import tp.projetpappl.items.Groupe;
+import tp.projetpappl.items.Seance;
 import tp.projetpappl.items.TypeLecon;
 import tp.projetpappl.repositories.ContientRepository;
+import tp.projetpappl.repositories.EnseignementRepository;
 import tp.projetpappl.repositories.GroupeRepository;
+import tp.projetpappl.repositories.SeanceRepository;
 
 /**
  *
@@ -31,6 +35,12 @@ public class ContientController {
     private GroupeRepository groupeRepository;
     @Autowired
     private ContientRepository contientRepository;
+    @Autowired
+    @Lazy
+    private SeanceRepository seanceRepository;
+    @Autowired
+    @Lazy
+    private EnseignementRepository enseignementRepository;
 
     @RequestMapping(value = "compteRenduGroupe.do", method = RequestMethod.POST)
     public ModelAndView handleIndexGet(HttpServletRequest request) {
@@ -43,16 +53,21 @@ public class ContientController {
         Groupe groupe;
         if (groupeOpt!=null){
             groupe=groupeOpt.get();
-            returned.addObject("groupe",groupe);
         
-            List<Enseignement> enseignements = contientRepository.findEnseignementByGroupe(groupe.getNomGroupe());
-            returned.addObject("enseignements",enseignements);
+            List<Enseignement> enseignements = contientRepository.findEnseignementByGroupe(groupe);
             
             List<List<TypeLecon>> intituleByEnseignement=getIntituleByEnseignement(groupe, enseignements);
-            returned.addObject("intitules",intituleByEnseignement);
             
             List<List<List<Contient>>> contients = getListContient(groupe, enseignements, intituleByEnseignement);
+            
+            List<Seance> listSeance = seanceRepository.findSeanceByGroupe(groupe);
+            seanceRepository.sortByEnseignementByIntitule(listSeance, enseignements, intituleByEnseignement);
+            
+            returned.addObject("groupe",groupe);
+            returned.addObject("enseignements",enseignements);
+            returned.addObject("intitules",intituleByEnseignement);
             returned.addObject("contients",contients);
+            returned.addObject("seances",listSeance);
             }
         return returned;
         }
@@ -61,7 +76,7 @@ public class ContientController {
             List<List<TypeLecon>> intituleByEnseignement=new ArrayList<>();
             if (!enseignements.isEmpty()){
                 for (Enseignement enseignement : enseignements){
-            List<TypeLecon> intitules = contientRepository.findIntituleByEnseignementByGroupe(enseignement.getAcronyme(), groupe.getNomGroupe());
+            List<TypeLecon> intitules = contientRepository.findIntituleByEnseignementByGroupe(enseignement, groupe);
             intituleByEnseignement.add(intitules);
                     }}
         return intituleByEnseignement;
@@ -72,11 +87,11 @@ public class ContientController {
             if (!intituleByEnseignement.isEmpty()){
                 for (Enseignement enseignement : enseignements){
                     List<List<Contient>> contientByEnseignement = new ArrayList<>();
-                    i++;
                     for (TypeLecon typeLecon : intituleByEnseignement.get(i)){
-            List<Contient> contientByType = contientRepository.findContientByIntituleByEnseignementByGroupe(typeLecon.getIntitule() ,enseignement.getAcronyme(), groupe.getNomGroupe());
+            List<Contient> contientByType = contientRepository.findContientByIntituleByEnseignementByGroupe(typeLecon ,enseignement, groupe);
             contientByEnseignement.add(contientByType);
                     }
+                    i++;
                     contients.add(contientByEnseignement);
                     }
             }
